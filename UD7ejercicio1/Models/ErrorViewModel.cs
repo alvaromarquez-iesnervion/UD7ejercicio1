e4198fbb50ea2a860ccd0a6698275223ff5b7114
alvaromarquez-iesnervion <alvaro.marquez@iesnervion.es>
2025-10-20 14:00:36 +0200

    Agregar archivos de proyecto.

---
+++ UD7ejercicio1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UD7ejercicio1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
