2ed1d056c56a4610ca9817c5d65d3d18a29c2315
alvaromarquez-iesnervion <alvaro.marquez@iesnervion.es>
2025-10-20 14:23:24 +0200

    Ejercicio completado

--- UD7ejercicio1/Controllers/HomeController.cs
+++ UD7ejercicio1/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using UD7ejercicio1.Models;
+using UD7ejercicio1.Models.Entities;
 
 namespace UD7ejercicio1.Controllers
 {
@@ -14,8 +15,23 @@ namespace UD7ejercicio1.Controllers
         }
 
         public IActionResult Index()
-        {
-            return View();
+        {   
+            if (DateTime.Now.Hour < 12)
+            {
+                ViewData["Greeting"] = "buenos dias!";
+            }
+            else if (DateTime.Now.Hour < 20)
+            {
+                ViewData["Greeting"] = "buenas tardes!";
+            }
+            else
+            {
+                ViewData["Greeting"] = "buenas noches!";
+            }
+
+            ViewBag.Date = DateTime.Now.ToLongDateString();
+            Persona Alvaro= new Persona(1, "Alvaro", "Marquez", 23);
+            return View(Alvaro);
         }
 
         public IActionResult Privacy()
---
+++ UD7ejercicio1/Models/Entities/clsPersona.cs
@@ -0,0 +1,33 @@
+﻿namespace UD7ejercicio1.Models.Entities
+{
+    /// <summary>
+    /// Representa a una persona con nombre, apellidos y fecha de nacimiento.
+    /// </summary>
+    public class Persona
+    {
+        
+        public int Id { get; }
+        public  string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Edad { get; set; }
+
+
+        // Constructor vacío
+        public Persona() { }
+
+        // Constructor con parámetros
+        public Persona(int id, string nombre, string apellido, int edad)
+        {
+            this.Id = id;
+            this.Nombre = nombre;
+            this.Apellido = apellido;
+            this.Edad = edad;
+        }
+
+        // Método para mostrar información
+        public void Presentarse()
+        {
+            Console.WriteLine($"Hola, me llamo {Nombre} {Apellido} y tengo {Edad} años.");
+        }
+    }
+}
